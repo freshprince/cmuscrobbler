ff19a8aec3cae2fca524d4845ad1d7f400ef4972
David Flatz <dflatz@student.ethz.ch>
2009-05-27 15:23:48 +0200

    Copyright updated

--- cmuscrobbler.py
+++ cmuscrobbler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # cmuscrobbler.py - Scrobble your Songs that you listened to in Cmus
-#    Copyright (C) 2008  David Flatz
+#    Copyright (C) 2008-2010  David Flatz
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
